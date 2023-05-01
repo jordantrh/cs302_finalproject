774f8e39bf9dee70fe7104cfc5651647de987625
atran8utk <92347696+atran8utk@users.noreply.github.com>
2023-05-01 17:19:02 -0400

    Update openAIWhisper.py

--- austin/openAIWhisper.py
+++ austin/openAIWhisper.py
@@ -1,6 +1,13 @@
+/*
+
+Make sure your python version is > 3.11. 
+This can be found by typing "python --version" in commandline
+
+*/
+
 import openai
 import whisper
 
-model = whisper.load_model("base.en")
+model = whisper.load_model("medium")
 result = model.transcribe("./test.mp3")
 print(result["text"])
